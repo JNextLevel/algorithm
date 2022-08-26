81b2b381c1751d06cb71586251a0f12d4b6a70ce
hunojung <huno0504@naver.com>
2022-08-27 00:53:31 +0900

    [정훈오] 1주차 목요일 과제 제출

---
+++ hunojung/src/boj/math/P01676.java
@@ -0,0 +1,33 @@
+package boj.math;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P01676 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count_two = 0;
+        int count_five = 0;
+        for(int i=2;i<=N;i++){
+
+            int count_num = i;
+            while(count_num%2 == 0 || count_num%5 == 0){
+                if(count_num%2 == 0){
+                    count_num /=2;
+                    count_two ++;
+                }
+                if(count_num%5 == 0){
+                    count_num /=5;
+                    count_five ++;
+                }
+            }
+        }
+
+        System.out.println(Math.min(count_two,count_five));
+
+        br.close();
+        System.exit(0);
+    }
+}
