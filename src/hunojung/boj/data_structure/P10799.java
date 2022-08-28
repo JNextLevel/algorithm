dc09117a00ad6c60b859400bc45aebfe601440f2
hunojung <huno0504@naver.com>
2022-08-28 22:25:56 +0900

    [정훈오] 1주차 화요일 과제 제출

---
+++ src/hunojung/boj/data_structure/P10799.java
@@ -0,0 +1,34 @@
+package hunojung.boj.data_structure;
+
+import java.io.*;
+import java.util.*;
+
+public class P10799 {
+        public static void main (String[]z) throws Exception{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringBuilder sb = new StringBuilder();
+
+            String s = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            int answer = 0;
+            for(int i = 0;i<s.length();i++) {
+                char c = s.charAt(i);
+                if(c=='(') {
+                    if(s.charAt(i+1)==')') {
+                        answer += stack.size();
+                        i++;
+                    }else {
+                        stack.push(c);
+                    }
+                }else {
+                    stack.pop();
+                    answer++;
+                }
+            }
+
+            System.out.println(answer);
+
+            br.close();
+            System.exit(0);
+        }
+    }
