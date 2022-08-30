9d120640553692e997cd29cafbf3adb233d36250
hunojung <huno0504@naver.com>
2022-08-30 16:56:08 +0900

    [정훈오] 2주차 일요일 과제

--- src/week02/백준_10974모든순열_hunojung.java
+++ src/week02/백준_10974모든순열_hunojung.java
@@ -1,10 +1,10 @@
-package week01;
+package week02;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 백준_15651N과M3_hunojung {
+public class 백준_10974모든순열_hunojung {
     static StringBuilder sb;
     static int N;
     public static void main (String[]z) throws Exception {
@@ -12,16 +12,16 @@ public class 백준_15651N과M3_hunojung {
         sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[N];
-        backTracking(arr,1,0);
+        boolean[] used_num = new boolean[N+1];
+        backTracking(arr,used_num,0);
         System.out.print(sb);
         br.close();
         System.exit(0);
     }
 
-    static void backTracking(int[] arr,int num, int idx){
+    static void backTracking(int[] arr,boolean[] used_num, int idx){
         if(idx == arr.length){
             for(int i=0;i<arr.length;i++){
                 sb.append(arr[i]+" ");
@@ -31,8 +31,12 @@ public class 백준_15651N과M3_hunojung {
         }
 
         for(int i=1;i<=N;i++){
-            arr[idx] = i;
-            backTracking(arr,i,idx+1);
+            if(!used_num[i]){
+                arr[idx] = i;
+                used_num[i] = true;
+                backTracking(arr,used_num,idx+1);
+                used_num[i] = false;
+            }
         }
     }
 }
--- src/week02/백준_9095123더하기_hunojung.java
+++ src/week02/백준_9095123더하기_hunojung.java
@@ -1,4 +1,4 @@
-package week01;
+package week02;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
